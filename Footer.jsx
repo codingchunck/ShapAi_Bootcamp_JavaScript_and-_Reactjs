a412c0e1fcd9a3dedf3b3266c4aec33702fb1c7b
codingchunck <85021517+codingchunck@users.noreply.github.com>
2021-06-27 19:18:13 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copy right by Bootcamps @ 2021</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript Bootcamp </h1>
+      <p>Basic web dev React JS Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
